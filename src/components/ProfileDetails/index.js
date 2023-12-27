3b025964c61239213253b66559f3c22d67f6444a
Teja Gulagani <143083469+tejababu846@users.noreply.github.com>
2023-12-27 21:27:23 +0530

    Update index.js

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -55,7 +55,7 @@ class ProfileDetails extends Component {
     return (
       <div className="profile-container">
         <img src={profileImageUrl} alt="profile" className="profile-logo" />
-        <h1 className="name-heading">{name}</h1>
+        <h1 className="name-heading">Teja Gulagani</h1>
         <p className="bio">{shortBio}</p>
       </div>
     )
